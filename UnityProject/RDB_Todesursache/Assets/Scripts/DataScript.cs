bdd900b482b2eeaa53dd1201507243dd2a4e8af0
chris22763 <cmuehrke@me.com>
2018-01-05 10:40:11 +0100

    3d files

---
+++ UnityProject/RDB_Todesursache/Assets/Scripts/DataScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataScript : MonoBehaviour {
+	public static DataScript instance; 
+	public int[] strArray = new int[100]; 
+	// Use this for initialization
+	void Start () {
+		instance = this;
+		catchData ();
+	}
+
+	void catchData(){
+		//toDo
+	}
+}
